cc22b6510e38d1963c725dd44eb42e0f1477f826
Garik <khvatik.igor@gmail.com>
2024-04-01 16:17:28 +0300

    add pydantic-settings, config.py

--- core/config.py
+++ core/config.py
@@ -1,7 +1,15 @@
 from pydantic import BaseModel
 from pathlib import Path
+from pydantic_settings import BaseSettings
 
 BASE_DIR = Path(__file__).parent.parent
+
+
+class Settings(BaseSettings):
+    db_url: str = ""
+
+
+settings = Settings()
 
 
 class AuthJWT(BaseModel):
